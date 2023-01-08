89b0dc9d83c950cb50ce6ffc2786d448149c5ad4
Nirmit Khurana <nirmitkhurana24@gmail.com>
2023-01-08 17:31:43 +0530

    Test commit in rough

--- src/javaDSA/rough.java
+++ src/javaDSA/rough.java
@@ -4,7 +4,7 @@ public class rough {
 
     public static void main(String[] args) {
 
-        System.out.println("This is my rough class");
+        System.out.println("I will practise here");
 
     }
 
